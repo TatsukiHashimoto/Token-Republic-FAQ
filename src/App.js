991f093d29162f85644bf9d19fa867045f20b517
Tatsuki Hashimoto <xhg3155@autuni.ac.nz>
2021-06-03 21:01:11 +1200

    Update
    Added Chatbot
    Fixed questions and answers
    Added service faq section

--- src/App.js
+++ src/App.js
@@ -93,6 +93,7 @@ function App() {
                   <Button href="#MetaMask">MetaMask</Button>
                   <Button href="#Investing">Investing</Button>
                   <Button href="#CrowdSale">Crowdsale</Button>
+                  <Button href="#Services">Services</Button>
                 </Box>
               </div>
               <div id="General">
@@ -121,21 +122,20 @@ function App() {
                 <AccordionFaq 
                   Topic={"Funds"}
                   Question = {"When and how can you draw out funds?"}
-                  Answer= {"You will receive funds to your metamask wallet as they are invested."}
+                  Answer= {"You will receive funds to your MetaMask wallet as they are invested."}
                 />
 
                 <AccordionFaq 
                   Topic={"Token Republic"}
                   Question = {"What is Token Republic? What can it do?"}
-                  Answer= {"Token Republic is an blockchain crowdfunding platforms that offers an efficient crowdfunding way for entrepreneurs and developers to build their business with fewer intermediaries, allowing them to focus on development."}
+                  Answer= {"Token Republic is a blockchain crowdfunding platforms that offers an efficient crowdfunding way for entrepreneurs and developers to build their business with fewer intermediaries, allowing them to focus on development."}
                 />
-
               </div>
               
               <div id="MetaMask">
                 <Divider/>
                 <Typography variant="h6" className={classes.topic}>
-                  Metamask Questions
+                  MetaMask Questions
                 </Typography>
 
                 <AccordionFaq 
@@ -148,13 +148,7 @@ function App() {
                 <AccordionFaq
                   Topic={"MetaMask"}
                   Question={"What is MetaMask?"}
-                  Answer={"Metamask is a cryptocurrency wallet that holding your token. Token Republic have collaborated with Metamask to help our client making transaction in a safer and more efficient way. "}
-                />
-
-                <AccordionFaq
-                  Topic={"Invest"}
-                  Question={"How do I use MetaMask to Invest?"}
-                  Answer={"When investing token, use Metamask to input your preferred number of tokens, and Metamask will send you the private key address that hold your tokens. Paste the private key address into Token Republic and complete the investment.  "}
+                  Answer={"MetaMask is a cryptocurrency wallet that holds your token. Token Republic have collaborated with MetaMask to help our client complete transaction in a safer and more efficient way. "}
                 />
               </div>
 
@@ -173,25 +167,13 @@ function App() {
                 <AccordionFaq
                   Topic="Setup"
                   Question={"How to start the CrowdSale?"}
-                  Answer={"After login in Token Republic dashboard. Click the “Crowdsale” button and set up crowdsale as required. "}
+                  Answer={"After logining in Token Republic dashboard. Click the “Crowdsale” button and set up crowdsale as required."}
                 />
 
                 <AccordionFaq
                   Topic={"Funding"}
                   Question={"If the funding has not achieved, could I still start my project?"}
                   Answer={"No, we are running “zero or all” crowdfunds system. If the funds not enough, the project will not start. "}
-                />
-
-                <AccordionFaq 
-                  Topic={"Rewards"}
-                  Question={"I alread invested in a project, where can I receive the rewards?"}
-                  Answer={"Difference project may contain different ways of rewarding methods, go to Project details and check the reward method. Once your reward result is out, you will receive that immediately in your Metamask accounts. "}
-                />
-
-                <AccordionFaq
-                  Topic={"Refund"}
-                  Question={"If the project fail, could I get my refund?"}
-                  Answer={"It depends, project may fail because they run out of budget or estimate the budget wrong. If the project fails by other situation, you may receive the refund within 3-15 working days."}
                 />
               </div>
 
@@ -203,7 +185,7 @@ function App() {
 
                 <AccordionFaq 
                   Topic={"Token and CrowdSale"}
-                  Question = {"Difference Between Token and Crowd sale token allocation?"}
+                  Question = {"Difference between Token and Crowdsale token allocation?"}
                   Answer= {"1. Is Total Supply of Token, this is the number of tokens that are issued when you create a token. You can use the same token on multiple crowdsales, required that the allocation of tokens to each crowdsale does not exceed the total supply of the token.\n\n"+
                   "2. The Crowdsale Token Allocation. This is how many tokens are available to buy via the crowdsale. This supply should equal the amount of funding you want for the crowdsale project i.e you supply 1,000,000 tokens to a crowdsale project these will be sold at the price per token you set."}
                 />
@@ -216,8 +198,8 @@ function App() {
                 
                 <AccordionFaq 
                   Topic={"Token"}
-                  Question={"How could I set up Token"}
-                  Answer={"After login to dashboard, there will be a “Token” button on the top left side of your dashboard. Click it to create your own token or modify it as your interest. "}
+                  Question={"How could I set up Token?"}
+                  Answer={"After login to dashboard, there will be a “Token” button on the top left side of your dashboard. Click it to create your own token or modify it as your interest."}
                 />
               </div>
 
@@ -230,18 +212,18 @@ function App() {
                 <AccordionFaq 
                   Topic={"CrowdSale"}
                   Question={"How could I invest in a CrowdSale?"}
-                  Answer={"First, you need to install Metamask, the application that holding your cryptocurrency. Then connect it with your Token republic account. After all, you are free to browse and invest in any interested crowdsales."}
+                  Answer={"First, you need to install MetaMask, the application that holds your cryptocurrency. Then it connects to Token republic account. After all, you are free to browse and invest in any interested crowdsales."}
                 />
 
                 <AccordionFaq
                   Topic={"Token"}
                   Question={"I don't have any required token, what should I do?"}
-                  Answer={"Metamask has a Swap token feature, use your exist token and swap them into the token that crowdsale required. Then you can successfully invest in crowdsale. "}
+                  Answer={"MetaMask has a Swap token feature, use your exist token and swap them into the token that crowdsale required. Then you can successfully invest in crowdsale."}
                 />
 
                 <AccordionFaq
                   Topic={"CrowdSale"}
-                  Question={"Why can I not invest in a seeding CrowdSale"}
+                  Question={"Why can I not invest in a seeding CrowdSale?"}
                   Answer={"Seeding crowdsales have 2 phases: Seeding and Funding. Seeding phase is to show your interest in the crowdsale. After that, you may invest the project when it became Funding phase."}  
                 />
 
@@ -256,6 +238,43 @@ function App() {
                   Question={"Where can I see how much interest has the project has gotten?"}
                   Answer={"Go to 'My Investment' and check out the current interest amount of the project."}
                 />  
+
+                <AccordionFaq 
+                  Topic={"Rewards"}
+                  Question={"I already invested in a project, where can I receive the rewards?"}
+                  Answer={"Different crowdsale may contain different ways of rewarding, go to Project details and check the reward method. Once your reward result is out, you will receive that immediately in your MetaMask accounts. "}
+                />
+
+                <AccordionFaq
+                  Topic={"Refund"}
+                  Question={"If the project fail, could I get my refund?"}
+                  Answer={"Once the project goes from Seeding phase to Investment stage then all funds are sent to the project owner. Should the project fail then investors would lose their funds unless the Project owner is offering some type of guarantee plan."}
+                />
+              </div>
+
+              <div id="Services">
+                <Divider/>
+                <Typography variant="h6" className={classes.topic}>
+                  Services
+                </Typography>
+
+                <AccordionFaq 
+                  Topic={"DWY"}
+                  Question={"What is DWY?"}
+                  Answer={"Do with you (DWY). For the user who have experience before but need some guide or help on some specific functions. Our team will guide you to solve the problem."}
+                />
+
+                <AccordionFaq
+                  Topic={"DIY"}
+                  Question={"What is DIY?"}
+                  Answer={"Do it yourself (DIY). For the user who are familiar with all the process and understand what they are doing without helping. Everything good to go."}
+                />
+
+                <AccordionFaq
+                  Topic={"DFY"}
+                  Question={"What is DFY?"}
+                  Answer={"Do it for you (DFY). For the user need our team to complete all the process for them. You have no idea what and how to do all things. Leave it to us, we will help you complete them."}  
+                />
               </div>
 
             </Container>
